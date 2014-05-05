f8efaea7fc0bc5a4f5b65cf2618762b4947d713c
arnaud-tcp <arnaud@touscoprod.com>
2014-05-05 21:05:27 +0200

    products

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -4,10 +4,10 @@ namespace Fkl\FranklinBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends Controller
+class AdminController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FklFranklinBundle:Home:index.html.twig');
+        return $this->render('FklFranklinBundle:Admin:index.html.twig');
     }
 }
--- Controller/ProduitController.php
+++ Controller/ProduitController.php
@@ -8,10 +8,25 @@ class ProduitController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FklFranklinBundle:Produit:index.html.twig');
+
+        
+       $em = $this->getDoctrine()->getManager();
+       $products = $em->getRepository('FklFranklinBundle:Product')->findAll();
+       $categories = $em->getRepository('FklFranklinBundle:Category')->findAll();
+       
+           $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $products = $em->getRepository('FklFranklinBundle:Product')->findByCategory($request->request->get('categorie'));
+
+            }
+       
+        return $this->render('FklFranklinBundle:Produit:index.html.twig', array('products' =>$products, 'categories' => $categories));
     }
     public function fullAction()
     {
-        return $this->render('FklFranklinBundle:Produit:full.html.twig');
+           $request = $this->getRequest();
+               $em = $this->getDoctrine()->getManager();
+                      $product = $em->getRepository('FklFranklinBundle:Product')->findOneById($request->query->get('id'));
+        return $this->render('FklFranklinBundle:Produit:full.html.twig', array('product'=>$product));
     }
 }
