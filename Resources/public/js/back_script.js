a2b7556caea0b3b63c92101724a14548b6f7e213
arnaud-tcp <arnaud@touscoprod.com>
2014-05-22 18:20:40 +0200

    date

--- Resources/public/js/back_script.js
+++ Resources/public/js/back_script.js
@@ -4,7 +4,7 @@ jQuery('#fkl_franklinbundle_intervention_date_from').datetimepicker({
   format:'Y-m-d H:i',
   onShow:function( ct ){
    this.setOptions({
-    maxDate:jQuery('#fkl_franklinbundle_intervention_date_to').val()?jQuery('#fkl_franklinbundle_intervention_date_to').val():false
+    maxDate:jQuery('#fkl_franklinbundle_intervention_date_to').val(),formatDate:'Y-m-d H:i'
    })
   },
  });
@@ -12,7 +12,7 @@ jQuery('#fkl_franklinbundle_intervention_date_from').datetimepicker({
   format:'Y-m-d H:i',
   onShow:function( ct ){
    this.setOptions({
-    minDate:jQuery('#fkl_franklinbundle_intervention_date_from').val()?jQuery('#fkl_franklinbundle_intervention_date_from').val():false
+    minDate:jQuery('#fkl_franklinbundle_intervention_date_from').val(),formatDate:'Y-m-d H:i'
    })
   },
  });
